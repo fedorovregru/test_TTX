c43b4ab1f2044974a86dac8981b0766579290796
Федоров Дмитрий <fedorov@reg.ru>
2017-02-18 22:31:18 +0400

    Добавил с помощью AUTOLOAD методы для работы с полями класса CrewMember.

--- CrewMember.pm
+++ CrewMember.pm
@@ -1,2 +1,39 @@
 package CrewMember;
 
+my %PROPERTIES = (
+	'CrewMember::fio'          => '',
+	'CrewMember::rank'         => '',
+	'CrewMember::profession'   => '',
+	'CrewMember::service_time' => '0',
+	'CrewMember::vehicle_type' => ''
+);
+
+use subs qw(fio rank profession service_time vehicle_type);
+
+sub new {
+	
+	my ( $invocant ) = @_;
+	
+	my $class = ref($invocant) || $invocant;
+	my $self  = { %PROPERTIES, @_ };
+	
+	return bless $self, $class;
+}
+
+sub AUTOLOAD {
+	
+	my $self = shift;
+	
+	exit unless ref($self);
+	my $name = our $AUTOLOAD;
+	return if $name =~ m/::DESTROY$/;
+	
+	if (@_) {
+		return $self->{$name} = shift;
+	}
+	else {
+		return $self->{$name};
+	}
+}
+
+return 1;
