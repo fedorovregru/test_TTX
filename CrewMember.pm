02c3c31951e308cba3b82eb9913a0ae024327046
Федоров Дмитрий <fedorov@reg.ru>
2017-02-19 13:06:47 +0400

    Завершил задание. Добавил прагму utf8 и установил вывод на STDOUT в кодировке utf8.

--- CrewMember.pm
+++ CrewMember.pm
@@ -1,10 +1,12 @@
 package CrewMember;
 
+use utf8;
+
 my %PROPERTIES = (
 	'CrewMember::fio'          => '',
 	'CrewMember::rank'         => '',
 	'CrewMember::profession'   => '',
-	'CrewMember::service_time' => '0',
+	'CrewMember::service_time' => '',
 	'CrewMember::vehicle_type' => ''
 );
 
@@ -22,18 +24,45 @@ sub new {
 
 sub AUTOLOAD {
 	
-	my $self = shift;
+	my ( $self, $value ) = @_;
 	
-	exit unless ref($self);
 	my $name = our $AUTOLOAD;
+	
+	# выход из метода если вызвал не объект класса
+	exit unless ref($self);
+	# выход из метода если вызван метод DESTROY
 	return if $name =~ m/::DESTROY$/;
 	
-	if (@_) {
-		return $self->{$name} = shift;
-	}
-	else {
+	# отдаем значение поля если нет входных параметров
+	return $self->{$name} unless $value;
+	
+	# проверка валидности поля fio
+	if ( $name =~ m/::fio$/ && !( $value =~ m/^.+\s.+\s.+$/ ) ) {
+		print "ФИО должно быть в три слова, через пробел.\n";
 		return $self->{$name};
 	}
+	
+	# проверка валидности поля rank
+	if ( $name =~ m/::rank$/ && !( $value =~ m/^\w+$/ ) ) {
+		print "Звание должно быть в одно слово.\n";
+		return $self->{$name};
+	}
+	
+	# проверка валидности поля profession
+	if ( $name =~ m/::profession$/
+	         && !( $value =~ m/^(командир|механик-водитель|наводчик|заряжающий|радист)$/ ) ) {
+		print "Специальность может быть только следующей:\n";
+		print "командир, механик-водитель, наводчик, заряжающий, радист.\n";
+		return $self->{$name};
+	}
+	
+	# проверка валидности поля service_time
+	if ( $name =~ m/::service_time$/ && !( $value =~ m/^\d+$/ ) ) {
+		print "Срок службы должен быть цифрой.\n";
+		return $self->{$name};
+	}
+	
+	return $self->{$name} = $value;
 }
 
 return 1;
